6d2ba3dda6fce460f40d100c28feab4b1b1f687e
sullung2yo@gmail.com <ec2-user@ip-172-31-1-6.ap-northeast-2.compute.internal>
2024-06-14 15:14:30 +0000

    Final

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,4 +7,4 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 # uWSGI: 유저가 보낸 데이터를 Nginx가 데이터를 받아오고, Django랑 소통을 하는 역활
-uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module config.wsgi
