6de5f4071b6a9120bf26bd9ce93f532f214e856a
Sullung <sullung2yo@gmail.com>
2024-06-14 22:41:11 +0900

    Modify settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'test')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0))) # 0: False 1: True
